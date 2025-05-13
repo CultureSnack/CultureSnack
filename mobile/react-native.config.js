72ec4229106c872f35f8abfc9b58cbeef104c3cc
BOOK-FKGCHLU3OD\jklun <jklun080921@gmail.com>
2025-05-14 01:55:35 +0900

    react native 환경 설정 중중

---
+++ mobile/react-native.config.js
@@ -0,0 +1,10 @@
+// react-native.config.js
+// module.exports = {
+//     project: {
+//         android: {
+//             compileSdkVersion: 36,
+//             targetSdkVersion: 36,
+//             buildToolsVersion: '33.0.0',
+//         },
+//     },
+// };
